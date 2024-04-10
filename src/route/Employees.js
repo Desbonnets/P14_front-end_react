930c80ac091a08015b4ad65c686f9e9758442f1d
Desbonnets <gillesdesbonnets@gmail.com>
2024-04-10 17:34:41 +0200

    test lib erreur css

--- src/route/Employees.js
+++ src/route/Employees.js
@@ -1,13 +1,56 @@
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { DataTable } from "p14_react_data_tables";
 
 function Employees() {
 
     const { employees } = useSelector((state) => state.employees);
     console.log(employees);
+    const employeesList = [
+        {
+            firstName: 'Elijah',
+            lastName: 'Larsen',
+            startDate: new Date('9/3/2006'),
+            department: 'Marketing',
+            dateOfBirth: new Date('12/26/1997'),
+            street: 'Chambers Alley',
+            city: 'Bridgeport',
+            state: 'Tennessee',
+            zipCode: '53584',
+        },
+        {
+            firstName: 'John',
+            lastName: 'Donovan',
+            startDate: new Date('9/1/2006'),
+            department: 'Sales',
+            dateOfBirth: new Date('7/17/1976'),
+            street: 'Monroe Tunnel',
+            city: 'San Antonio',
+            state: 'Florida',
+            zipCode: '10494',
+        },
+      ];
+      
+      const employeesHeader = [
+        {id: 'firstName', label: 'First Name', type: 'string'},
+        {id: 'lastName', label: 'Last Name', type: 'string'},
+        {id: 'startDate', label: 'Start Date', type: 'date'},
+        {id: 'department', label: 'Department', type: 'string'},
+        {id: 'dateOfBirth', label: 'Date of Birth', type: 'date'},
+        {id: 'street', label: 'Street', type: 'string'},
+        {id: 'city', label: 'City', type: 'string'},
+        {id: 'state', label: 'State', type: 'string'},
+        {id: 'zipCode', label: 'Zip Code', type: 'string'}
+    ]
     return (
         <div>
             <h1>Current Employees</h1>
+            <DataTable 
+                data={employeesList} 
+                header={employeesHeader} 
+                enableSearch={true} 
+                enablePagination={true} 
+            />
             <table id="employee-table" className="display">
                 <thead>
                     <tr>
